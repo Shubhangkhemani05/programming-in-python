a27f36c7d99e307370c7d2bc4baba363a1688419
Shubhangkhemani05 <20cs027@charusat.edu.in>
2022-02-26 21:49:11 +0530

    practical_5 PIP

---
+++ practical_5/main.py
@@ -0,0 +1,20 @@
+string = input("Enter string: ")
+output_string = ""
+for ch in string:
+    if ch.islower():
+        output_string += ch.upper()
+    else:
+        output_string += ch.lower()
+
+print("Swapped: "+output_string)
+
+string = input("Enter string: ")
+output_string = ""
+for ch in string:
+    if ch.islower():
+        output_string += ch.upper()
+    else:
+        output_string += ch.lower()
+
+print("Swapped: "+output_string)
+
